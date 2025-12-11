78217d86ceace37e638e8978a8a02d77dc9708ae
Harsh vardhan <harshvardhan7112@gmail.com>
2025-12-11 15:04:49 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (80.90%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        long long result = 0;
+        bool sign = false;
+        int i = 0;
+        while(i < s.size() && s[i] == ' ')
+        {
+            i++;
+        }
+        if(i < s.size() && (s[i] == '-' || s[i] == '+')) {
+            if(s[i] == '-') 
+                sign = true;
+            i++;
+        }
+        while(i < s.size() && isdigit(s[i])) {
+            int no = s[i] - '0';
+            if(result > (INT_MAX - no) / 10) {
+                return sign ? INT_MIN : INT_MAX;
+            }
+            result = result * 10 + no;
+            i++;
+        }
+
+        return sign ? -result : result;
+    }
+};
