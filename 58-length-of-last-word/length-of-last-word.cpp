6353c7dd5d154191f494bb6a4be64ac40b17ef1c
Harsh vardhan <harshvardhan7112@gmail.com>
2025-12-18 18:31:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.09%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        if(s.empty())   return 0;
+        int k=0;
+        for(int i=0;i<s.length();i++){
+            int a = s[i];
+            int b = s[i+1];
+            if((64<a && a<123))
+                k++;
+            else if(a==32 && (64<b && b<123))
+                k=0;
+
+        }
+        return k;
+    }
+};
