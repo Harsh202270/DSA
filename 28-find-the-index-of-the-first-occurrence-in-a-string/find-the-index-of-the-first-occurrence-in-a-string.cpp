67e62bed877c00de2832f001fbc3d259f2caec1b
Harsh vardhan <harshvardhan7112@gmail.com>
2025-12-23 00:08:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (14.00%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,41 @@
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         if(needle.empty()||haystack.empty())
+//             return 0;
+//         for(int i=0 ; i<haystack.length()-needle.length()+ 1 ; i++)
+//         {
+            
+//             int j;
+//             for(j=0;j<needle.length();j++)
+//             {
+//                 if(haystack[i+j] != needle[j])
+//                     break;
+//             }
+//             if(j==needle.length())
+//                 return i;
+//         }
+//     return -1;
+//     }
+// };
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) return 0;
+        if (needle.size() > haystack.size()) return -1;
+
+        for(int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j;
+            for(j = 0; j < needle.length(); j++)
+            {
+                if(haystack[i + j] != needle[j]){
+                    break ;
+                }
+            }
+            if(j == needle.length())
+                return i;
+        }
+        return -1;
+    }
+};
