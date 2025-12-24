68f536ad349c9428bcd0222f5e6a5a5d88322a4e
Harsh vardhan <harshvardhan7112@gmail.com>
2025-12-24 23:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (75.17%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int value(char c) {
+        switch(c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+    int romanToInt(string s) {
+        int result = 0;
+        for (int i=0 ; i<s.size();i++)
+        {
+            int curr = value(s[i]);
+            int next;
+            if (i + 1 < s.size()) {
+                next = value(s[i + 1]);
+            } 
+            else {
+                next = 0;
+            }    
+            if(curr < next){
+                result = result -curr;
+            }
+            else{
+                result = result + curr;
+            }
+        }
+        return result;
+    }
+};
