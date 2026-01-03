8df3fc69e1388d4e7f53d8fe91de11e010a4972f
Harsh vardhan <harshvardhan7112@gmail.com>
2026-01-03 22:51:35 +0530

    Time: 5 ms (20.22%) | Memory: 8.5 MB (53.58%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,50 @@
+// class Solution {
+// public:
+//     int reverse(int x) {
+//         int ans=0;
+        
+//         if(x<0)
+//         {
+//             x = abs(x);
+//             for(int i=0;x>0;x=x/10)
+//             {
+//                 if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10)) {
+//                     return 0;
+//                 }   
+//                 ans = (ans *10) + (x%10);
+//             }
+//             return -ans;
+//         }
+//         else
+//         {
+//             for(int i=0;x>0;x=x/10)
+//             {
+//                 if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10)) {
+//                     return 0;
+//                 }
+//                 ans = (ans *10) + (x%10);
+//             }
+//             return ans;
+//         }
+//         return 0;
+//     }
+// };
+
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0; // Initialize the reversed number to 0
+        while (x != 0) {
+            int digit = x % 10; // Get the last digit of x
+            
+            // Check for overflow/underflow before updating ans
+            if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10)) {
+                return 0; // Return 0 if reversing x would cause overflow/underflow
+            }
+            
+            ans = ans * 10 + digit; // Append the digit to the reversed number
+            x = x / 10; // Remove the last digit from x
+        }
+        return ans; // Return the reversed number
+    }
+};
