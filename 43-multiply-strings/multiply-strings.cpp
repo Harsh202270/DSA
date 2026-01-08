c7c075a08502de4f258b96e911f6349a01163ff1
Harsh vardhan <harshvardhan7112@gmail.com>
2026-01-08 23:38:51 +0530

    Time: 3 ms (59.32%) | Memory: 10.8 MB (14.86%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        // int num11 = stoi(num1);
-        // int num22 = stoi(num2);
-        // long long result = num11 * num22;
-        // string ans = to_string(result);
-        // return ans;
         if(num1 == "0" || num2 == "0") return "0";
         reverse(num1.begin(),num1.end());
         reverse(num2.begin(),num2.end());
