206c4b04bc95a122c19fcdcf13e0f3fe8d9742d1
Harsh vardhan <harshvardhan7112@gmail.com>
2026-01-30 21:45:24 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (54.35%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        int count=0;
+        for(int i=0 ; i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[j]=nums[i];
+                j++;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        for(int i=0 ; i<count;i++)
+        {
+            nums[j]=0;
+            j++;
+        }
+    }
+};
