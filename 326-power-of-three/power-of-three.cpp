5312e68b9af0acf2312d56df0ce325b81877b549
Harsh vardhan <harshvardhan7112@gmail.com>
2026-01-31 11:55:16 +0530

    Time: 4 ms (44.64%) | Memory: 8.8 MB (96.89%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -4,9 +4,7 @@ public:
         if(n==0)
             return false;
         while(n!=0 && n%3==0)
-        {
             n = n/3;
-        }
         if(n==1)
             return true;
         return false;
