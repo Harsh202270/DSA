1e9b24acecba8c9f99fe1dff6f444a144a739f58
Harsh vardhan <harshvardhan7112@gmail.com>
2026-02-27 23:36:17 +0530

    Update count-primes.cpp

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -3,7 +3,7 @@ public:
     int countPrimes(int n) {
         int cnt = 0;
         vector<bool> prime(n+1, true);
-
+        int ans;
         prime[0] = prime[1] = false;
 
         for(int i=2; i<n; i++) {
