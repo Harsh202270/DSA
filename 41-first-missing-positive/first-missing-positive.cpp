24fb77b462003d8f946387e375c22ff4a39c0856
Harsh vardhan <harshvardhan7112@gmail.com>
2026-03-11 22:41:13 +0530

    Time: 1 ms (59.81%) | Memory: 55 MB (84.90%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        // Step 1: Replace all numbers <= 0 or > n with n+1
         for (int i = 0; i < n; ++i) {
             if (nums[i] <= 0 || nums[i] > n) {
                 nums[i] = n + 1;
             }
         }
 
-        // Step 2: Use index as a marker
         for (int i = 0; i < n; ++i) {
             int val = abs(nums[i]);
             if (val >= 1 && val <= n) {
-                nums[val - 1] = -abs(nums[val - 1]); // Mark as negative
+                nums[val - 1] = -abs(nums[val - 1]); 
             }
         }
 
-        // Step 3: First index with positive value is the missing number
         for (int i = 0; i < n; ++i) {
             if (nums[i] > 0) {
                 return i + 1;
