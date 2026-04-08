591e1d12f69733838dc1780116648204d6418026
Harsh vardhan <harshvardhan7112@gmail.com>
2026-04-08 18:52:37 +0530

    Time: 3 ms (38.90%) | Memory: 8.9 MB (34.69%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        if(n==1)
+            return 1;
+        int i=1;
+        while(n>=0)
+        {
+            n = n - i;
+            i++;
+        }
+        return i-2;
+    }
+};
